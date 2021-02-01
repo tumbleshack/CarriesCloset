4f36d05c71f1fd8955eac710096478040b1a3d98
juliagrigni <juliagrigni@gmail.com>
2021-02-01 15:02:49 -0500

    populated dropdowns and fixed phone type storage

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,17 +1,31 @@
 class Request < ApplicationRecord
   URGENCIES = {
     '': 0,
-    'Within 24 hours': 1
+    'Within 24 hours': 1,
+    'Within 48 hours': 2,
+    'Within a week': 3,
   }.freeze
 
   RELATIONSHIPS = {
     '': 0,
-    'Foster Child': 1
+    'Foster Child': 1,
+    'Juvenile Detention Center': 2,
+    'Domestic Violence Center': 3,
+    'Homeless Youth': 4,
+    'Refugee/Displaced Person': 5,
   }.freeze
 
   COUNTIES = {
     '': 0,
-    'Fulton': 1
+    'Fulton': 1,
+    'Dekalb': 2,
+    'Gwinnet': 3,
+    'Cobb': 4,
+    'Clayton': 5,
+    'Henry': 6,
+    'Muscogee': 7,
+    'Other (provide your address below)': 8
+    
   }.freeze
 
   validates_presence_of :relationship, :full_name, :urgency, :email, :availability,
