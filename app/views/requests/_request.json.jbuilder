cb064546aa7ee4d32d9d178e420baf93000ff591
Lincoln Anders <lincolnanders5@gmail.com>
2021-02-01 14:38:18 -0500

    Add Request (#30)
    * Generated `Request` scaffold
    * Added initial validation and link to form
    * Changed form copy to show Figma copy
    * `type` is a reserved field in Rails
    * Migrated from `type` to `relationship` to describe the type of donation.
    * Added enum-based fields
    * Added link to home page
    * Added link to home page

---
+++ app/views/requests/_request.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! request, :id, :urgency, :full_name, :email, :phone, :relationship, :county, :meet, :address, :availability, :comments, :created_at, :updated_at
+json.url request_url(request, format: :json)
---
+++ db/migrate/20210129013218_create_requests.rb
@@ -0,0 +1,18 @@
+class CreateRequests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :requests do |t|
+      t.integer :urgency
+      t.string :full_name
+      t.string :email
+      t.integer :phone
+      t.integer :relationship
+      t.integer :county
+      t.boolean :meet
+      t.string :address
+      t.string :availability
+      t.text :comments
+
+      t.timestamps
+    end
+  end
+end
