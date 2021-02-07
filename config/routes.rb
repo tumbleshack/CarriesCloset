983a5df6d2b13d3edc3dad510332ad0b6dd5762f
Nicholas Grana <grananicholas@gmail.com>
2021-02-06 20:57:16 -0500

    initial route configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get '/requests/my_requests' => 'requests#my_requests'
   resources :requests
   get 'public/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "public#index"
+  root to: "public#index"  
 end
