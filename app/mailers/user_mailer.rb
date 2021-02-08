3f680bb155f36f8904ac610e41f04285a5ed760b
Farooqui, Nooreen <nfarooqui7@gatech.edu>
2021-02-08 18:33:46 -0500

    created new email type for urgent admin emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,15 @@
 class UserMailer < ApplicationMailer
-
+    def new_admin_urgent_email
+        # UserMailer.with(user: User.first).welcome_email
+        @request = params[:request]
+        mail to: ENV[ADMIN_EMAIL],
+          subject: "[URGENT] Donation Request"
+    end
 
     def new_email
         # UserMailer.with(user: User.first).welcome_email
         @request = params[:request]
         mail to: @request.email,
           subject: "Donation Request Confirmation"
-      end
+    end
 end
