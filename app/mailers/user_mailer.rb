ec196a80fece3df95dd8c136f1d46d3d141e9141
Farooqui, Nooreen <nfarooqui7@gatech.edu>
2021-02-08 18:42:10 -0500

    created txt and html files to draft new admin email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
     def new_admin_urgent_email
         # UserMailer.with(user: User.first).welcome_email
         @request = params[:request]
-        mail to: ENV[ADMIN_EMAIL],
+        mail to: ENV["ADMIN_EMAIL"],
           subject: "[URGENT] Donation Request"
     end
 
