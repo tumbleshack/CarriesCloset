b012b4c38f10dea241a628bb3c3090073d41287d
nooreenfarooqui <54963769+nooreenfarooqui@users.noreply.github.com>
2021-02-15 13:04:55 -0500

    Add Roles (#57) (#58)
    * Add all role fields
    
    * Added convenience scopes and default admin user
    
    * Created route protection method
    
    * Added convenience link to all requests if admin
    
    reviewed by Nooreen
    
    Co-authored-by: Lincoln Anders <lincolnanders5@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_013218) do
+ActiveRecord::Schema.define(version: 2021_02_09_003059) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,10 @@ ActiveRecord::Schema.define(version: 2021_01_29_013218) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
+    t.boolean "volunteer", default: false
+    t.boolean "donee", default: false
+    t.boolean "donor", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
