50d66322388f5be4e6540e045e33eef4986b8d6b
Farooqui, Nooreen <nfarooqui7@gatech.edu>
2021-02-15 15:14:42 -0500

    trying to fix no admin emails error

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,18 @@ class UserMailer < ApplicationMailer
     def new_admin_urgent_email
         # UserMailer.with(user: User.first).welcome_email
         @request = params[:request]
-        mail to: User.admins.pluck(:email), #send to all admins
-          subject: "[URGENT] Donation Request" if User.admins.any? # only is admins exist
+        # mail to: User.admins.pluck(:email), #send to all admins
+        #   subject: "[URGENT] Donation Request" if User.admins.any? # only is admins exist
+        if User.admins.any?
+          mail to: User.admins.pluck(:email), #send to all admins
+           subject: "[URGENT] Donation Request"
+        else 
+          mail to: 'carries.closet.confirmations@gmail.com', #send to all admins
+           subject: "[URGENT] Donation Request"
+        end
+        
+      
+        
     end
 
     def new_email
