1f6419d49e848c087d0e01d97c4b5ae2f0755b84
tumbleshack <bmckaym@gmail.com>
2021-02-18 19:03:50 -0500

    Sign up invite links worked out of the box. Role selection still to be implemented.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   # Adds in several different ways to query all `User` objects. For example, to
