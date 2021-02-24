a0023cdc5ea9e49c6c6a5e1bcc4a1909b1b70445
tumbleshack <bmckaym@gmail.com>
2021-02-24 17:17:35 -0500

    Added roles to the invite page & customized email

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,3 +1,12 @@
 class Users::InvitationsController < Devise::InvitationsController
     before_action -> { require_role(:root, :admin) }, only: [:new, :create]
+    before_action :configure_permitted_parameters
+
+    protected
+
+    def configure_permitted_parameters
+        puts "params configured"
+        devise_parameter_sanitizer.permit(:invite, keys: [:admin, :volunteer, :donor, :donee])
+        # devise_parameter_sanitizer.permit(:accept_invitation, keys: [:admin, :volunteer, :donor, :donee])
+    end
 end
