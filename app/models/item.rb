7ba732f267611261e273372a11e99a25f52c79e5
Lincoln Anders <lincolnanders5@gmail.com>
2021-02-26 00:19:44 -0500

    Break category into separate object

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,5 @@
 class Item < ApplicationRecord
-  
-  CATEGORIES = {
-    '': 0,
-    'Boys': 1,
-    'Girls': 2,
-    'Womens': 3,
-    'Mens': 4,
-    'Hygeine Products': 5
-  }.freeze
+  has_one :category
   
   validates_presence_of :quantity, :category, :itemType, :size
 
