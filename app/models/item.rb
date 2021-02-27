6c695462f0009cf25b13ea0b3de2e51546133802
juliagrigni <juliagrigni@gmail.com>
2021-02-26 21:22:55 -0500

    delete is working in requests, took out uniqueness constraint that was erroring

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,6 @@ class Item < ApplicationRecord
   validates_presence_of :quantity, :itemType, :size
 
   validates_numericality_of :quantity, greater_than: -1, :message => "can't be negative"
-  
-  validates_uniqueness_of :itemType, :scope => [:category, :size]
 
   def category
     Category.find(category_id)
