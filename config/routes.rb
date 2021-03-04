97f1a0794d87a5bb1ded6e899d4f7495e29d477f
Farooqui, Nooreen <nfarooqui7@gatech.edu>
2021-03-04 17:29:27 -0500

    added user\manage route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :items
   get '/requests/my_requests' => 'requests#my_requests'
+  get '/users/manage' => 'users#manage'
   resources :requests
   get 'public/index'
   devise_for :users, controllers: { invitations: 'users/invitations' }
