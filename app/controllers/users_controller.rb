6d1bc4efdc1b76a0f3c4388de5c4163059e11353
Farooqui, Nooreen <nfarooqui7@gatech.edu>
2021-03-04 17:53:42 -0500

    added all users as parameter to user data page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
     
     def new
+        @users = User.all
         render 'users/manage'
     end
 
     def index
+        @users = User.all
         render 'users/manage'
     end
     
     def manage
+        @users = User.all
         render 'users/manage'
     end
   end
