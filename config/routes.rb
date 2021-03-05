8ddcb69fa8dc049d7ead469caa4a63cea21274a0
Farooqui, Nooreen <nfarooqui7@gatech.edu>
2021-03-05 15:14:42 -0500

    allowed for routing to individual user info based on id

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get '/users/manage' => 'users#manage'
   resources :requests
   get 'public/index'
-  devise_for :users, controllers: { invitations: 'users/invitations' }
+  devise_for :users, :path_prefix => 'my', controllers: { invitations: 'users/invitations' }
+  resources :users
+   
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "public#index"  
 end
