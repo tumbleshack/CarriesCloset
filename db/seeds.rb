8640f848e2699e71858f2ab14fd2f820a7530ee8
tumbleshack <bmckaym@gmail.com>
2021-03-16 01:17:43 -0400

    All displays updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,20 +24,4 @@ categories.each do |category|
 @someitem = Item.find_by_itemType("Shoes")
 @someitem = Item.create!(quantity: 2, category: Category.find_by_name(categories.first), itemType: "Shoes", size: "6") if @someitem.nil?
 
-# loop do
-#   break if Request.count >= 10
-
-#   @request = Request.create! urgency: (1...Request::URGENCIES.count).to_a.sample,
-#                              full_name: Faker::Name.name,
-#                              email: Faker::Internet.email,
-#                              phone: (678_555_0000...678_567_9999).to_a.sample.to_s,
-#                              relationship: (1...Request::RELATIONSHIPS.count).to_a.sample,
-#                              county: (1...Request::COUNTIES.count).to_a.sample,
-#                              meet: 1,
-#                              availability: Faker::Lorem.sentence,
-#                              comments: Faker::Lorem.sentence
-#                             #  items: Item.create!(quantity: 1, category: Category.find_by_name(category), itemType: category, size: "M", in_inventory: false, in_request: true, in_donation: false)
-#   puts "Request #{Request.count}: '#{@request.full_name}' requests '#{@request.items}'"
-# end
-
 end
