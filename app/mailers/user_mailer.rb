99dfdb76f7318a38e8cb475da56ad6d01602ad78
Farooqui, Nooreen <nfarooqui7@gatech.edu>
2021-03-26 14:29:32 -0400

    added new function for volunteer emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,9 +11,30 @@ class UserMailer < ApplicationMailer
         else 
           mail to: 'carries.closet.confirmations@gmail.com', #send to all ccarries closet email if no admins
            subject: "[URGENT] Donation Request"
+        end   
+    end
+
+    def volunteer_email
+      @request = params[:request]
+      volunteer_emails = Array.new
+      count = 0
+      if User.volunteers.any?
+        volunteers = User.volunteers.pluck(:email)
+        for x in User.volunteers do 
+          if x.email_setting.send_all?
+            volunteer_emails[count] = x.email
+            count = count + 1
+          end  
         end
-       
-        
+      end
+      
+      if volunteer_emails.length() > 0
+        mail to: volunteer_emails, #send to all admins
+            subject: "Donation Request Notification"
+      else 
+        mail to: 'carries.closet.confirmations@gmail.com', #send to all ccarries closet email if no admins
+          subject: "Donation Request Notification"
+      end 
     end
 
 
