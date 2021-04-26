0c275bfb8d1453fd271df41c9fb7799bfe426854
Lincoln Anders <lincolnanders5@gmail.com>
2021-04-26 13:25:55 -0400

    fixed updating email preference

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,20 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :create_email_settings, only: :edit
+
+  def update
+    attrs = user_params[:email_setting_attributes]
+    @user.email_setting.preference = attrs[:preference] unless attrs.nil? || @user.email_setting.nil?
+    super
+  end
 
   protected
+
+  def create_email_settings
+    if !@user.nil? && @user.email_setting.nil?
+      @user.build_email_setting(preference: :'All Urgent Requests').save!
+    end
+  end
 
   def set_user
     @user = current_user
